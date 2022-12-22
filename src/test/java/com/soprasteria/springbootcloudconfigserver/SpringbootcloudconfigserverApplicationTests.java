a305b20bed7b339bad173df3b6fbb4c7274918f7
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-12-22 18:58:35 +0100

    spring cloud config server

---
+++ src/test/java/com/soprasteria/springbootcloudconfigserver/SpringbootcloudconfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soprasteria.springbootcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootcloudconfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
